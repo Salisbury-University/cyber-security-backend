f7cd9bfd77f87453cf8c54bd3eef0182a0c34caa
JungAn2 <jungan1991@gmail.com>
2022-03-30 15:49:07 -0400

    Changed some issues

--- app/services/AuthService.ts
+++ app/services/AuthService.ts
@@ -1,30 +1,33 @@
-import { PrismaClient } from '.prisma/client';
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
+import JwtMalformedException from "../exceptions/JwtMalformedException";
 
-const prisma = new PrismaClient()
 /**
  * All of the functions regarding authorization
  */
 export const AuthService = {
-	/**
-	 * Validates an authorization token for authentication.
-	 *  
-	 * @param {String} token Authorization token attached to the HTTP header.
-	 * @return {boolean} True if their token is valid, false if it isn't.
-	 */
-	validate(token: String): boolean {
-		if (token.length != 0) {
-			return true;
-		}
-		return false;
-	},
-	/**
-	 * Decode the Jsonwebtoken
-	 * 
-	 * @param {String} token Authorization token attached to the HTTP header
-	 * @returns {JSON} Decoded jsonwebtoken
-	 */
-	decodeToken(token: String): JSON{
-		return jwt.decode(token, {json: true})
-	},
-}
+  /**
+   * Validates an authorization token for authentication.
+   *
+   * @param {String} token Authorization token attached to the HTTP header.
+   * @return {boolean} True if their token is valid, false if it isn't.
+   */
+  validate(token: String): boolean {
+    if (token.length != 0) {
+      return true;
+    }
+    return false;
+  },
+  /**
+   * Decode the Jsonwebtoken
+   *
+   * @param {String} token Authorization token attached to the HTTP header
+   * @return {JSON} Decoded jsonwebtoken
+   */
+  decodeToken(token: String): JSON {
+    const PAYLOAD: JSON = jwt.decode(token, { json: true });
+    if (PAYLOAD === null) {
+      throw new JwtMalformedException();
+    }
+    return PAYLOAD;
+  },
+};
