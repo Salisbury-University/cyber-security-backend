f2b0a60951fdc34aa892fa52c6e5e94756096877
JungAn2 <jungan1991@gmail.com>
2022-03-30 16:06:26 -0400

    Added decoded token exception

--- tests/AuthService.spec.ts
+++ tests/AuthService.spec.ts
@@ -1,30 +1,40 @@
-import { test } from '@japa/runner'
+import { test } from "@japa/runner";
 import { AuthService } from "../app/services/AuthService";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
+import JwtMalformedException from "../app/exceptions/JwtMalformedException";
 
-test.group('AuthService', () => {
+test.group("AuthService", () => {
   /**
    * Basic token data
    */
   const DATA = {
     u_id: "test1",
-    password: "password"
-  }
-  const SECRET_TOKEN:String = "Secret"
-  const TOKEN:String = jwt.sign(DATA, SECRET_TOKEN)
+    password: "password",
+  };
+
+  const SECRET_TOKEN: String = "Secret";
+  const TOKEN: String = jwt.sign(DATA, SECRET_TOKEN);
 
   /**
    * This is place holder for now. Once LDAP is able to run will replace this
    */
-  test('Token validation', ({ expect }) => {
+  test("Token validation", ({ expect }) => {
     // Test logic goes here
     expect(AuthService.validate("Hello world")).toEqual(true);
-  })
+  });
 
   /**
    * Testing JWT decode function.
    */
-  test('JWT_decode', ({expect})=>{
-    expect(AuthService.decodeToken(TOKEN)).toMatchObject(DATA)
-  })
-})
+  test("JWT_decode", ({ expect }) => {
+    expect(AuthService.decodeToken(TOKEN)).toMatchObject(DATA);
+  });
+
+  test("JWT_decode_Error", ({ expect }) => {
+    try {
+      AuthService.decodeToken(TOKEN.replace("i", "vert"));
+    } catch (e) {
+      expect(e).toBeInstanceOf(JwtMalformedException);
+    }
+  });
+});
