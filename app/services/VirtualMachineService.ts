0f2297ced5b2493bc2b21fbd69a6739179b81754
JungAn2 <jungan1991@gmail.com>
2022-04-06 15:11:01 -0400

    Changed route of NotFoundException

---
+++ app/services/VirtualMachineService.ts
@@ -0,0 +1,34 @@
+import { PrismaClient, VM } from ".prisma/client";
+import UnauthorizedException from "../exceptions/UnauthorizedException";
+import NotFoundException from "../exceptions/NotFoundException";
+
+const prisma = new PrismaClient();
+
+export const VirtualMachineService = {
+  async checkPermission(id: string, user: string): Promise<VM> {
+    const VM = await prisma.vM.findFirst({
+      where: {
+        vm: id,
+      },
+    });
+    // Excercise does not exist
+    if (VM == null) throw new NotFoundException();
+
+    // Check if the vm is started by user
+    // User permission
+    // Note: Admin cannot be added yet until
+    // Jwt code returns group/admin
+    if (VM.user != user) throw new UnauthorizedException();
+
+    return VM;
+  },
+
+  async getVM(id: string, user: string): Promise<VM> {
+    try {
+      const VM = await this.checkPermission(id, user);
+      return VM;
+    } catch (e) {
+      return e;
+    }
+  },
+};
