477d448ca4aea74e93ef4a75a5c134443cffbe82
lamont1008 <kjenifer1@gulls.salisbury.edu>
2022-04-10 13:17:56 -0400

    updated

--- tests/ValidationMiddleware.spec.ts
+++ tests/ValidationMiddleware.spec.ts
@@ -1,28 +1,28 @@
-// import { test } from '@japa/runner'
-// import request  from 'supertest'
-// import {app} from '../app'
-// import jwt from 'jsonwebtoken'
+import { test } from '@japa/runner'
+import request  from 'supertest'
+import {app} from '../app'
+import jwt from 'jsonwebtoken'
 
-// test.group('ValidationMiddleware', () => {
+test.group('ValidationMiddleware', () => {
 
-// const body = {
-//     username : "cxarausa",
-//     password : "testing"
-// }    
-// /**
-//      * Testing the registration of user api call
-//      */
-//  test('/Login', async({expect}, done:Function)=>{
-//     request(app)
-//         .post('/auth/login')
-//         .set('Accept', 'application/json')
-//         .expect(200)
-//         .send(body)
-//         .then(async(res)=>{
-//             const decode = jwt.decode(res.text, {json: true})
-//             expect(decode).toMatchObject({uid: body.username})
-//             done()
-//         })
-// }).waitForDone()
+const body = {
+    username : "cxarausa",
+    password : "testing"
+}    
+/**
+     * Testing the registration of user api call
+     */
+ test('/Login', async({expect}, done:Function)=>{
+    request(app)
+        .post('/auth/login')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .send(body)
+        .then(async(res)=>{
+            const decode = jwt.decode(res.text, {json: true})
+            expect(decode).toMatchObject({uid: body.username})
+            done()
+        })
+}).waitForDone()
 
-// })
+})
