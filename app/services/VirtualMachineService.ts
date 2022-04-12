b45013791d1e7516195032a3c1a9c6198add0245
JungAn2 <jungan1991@gmail.com>
2022-04-11 22:19:57 -0400

    added some functions

--- app/services/VirtualMachineService.ts
+++ app/services/VirtualMachineService.ts
@@ -4,8 +4,14 @@ import axios from "axios";
 const prisma = new PrismaClient();
 
 export const VirtualMachineService = {
+  /**
+   * Check if VM timeEnd with current time
+   *
+   * @param {VM} VM virtual machine databse for currently running
+   * @returns
+   */
   checkTimeEnd(VMinfo: VM): boolean {
-    if (VMinfo.timeEnd > new Date()) return false;
+    if (VMinfo.timeEnd < new Date()) return false;
     return true;
   },
 
@@ -20,6 +26,8 @@ export const VirtualMachineService = {
       // Double checking and terminating if VM is running
       if (this.checkTimeEnd(VM)) {
         //Terminate the VM
+      } else {
+        //Throw error
       }
     }
   },
@@ -28,19 +36,39 @@ export const VirtualMachineService = {
 
   checkConnection(user: string) {},
 
-  cloneTemplate(vmid: number) {
-    const load = this.checkNodeLoad();
+  cloneTemplate(vmid: string) {
+    const load = this.selectNodeLoad();
+    const newid = "1000";
     axios.post(
-      server.concat("/api2/json/nodes/", load, "/qemu/", vmid, "/clone"),
-      {
-        newid: 200,
-      }
+      process.env.NODE.concat(
+        "/api2/json/nodes/",
+        load,
+        "/qemu/",
+        vmid,
+        "/clone?newid=",
+        newid
+      )
     );
   },
   /**
-   * Checks the load of each server
+   * Checks the load by memory
    */
-  checkNodeLoad(): string {
-    axios.post(process.env.NODE.concat("/api2/json/nodes/")).then((res) => {});
+  selectNodeLoad(): any {
+    return axios
+      .post(process.env.NODE.concat("/api2/json/nodes"))
+      .then((res) => {
+        let node: any = { node: res.data[0].node, mem: res.data[0].mem };
+        // If node is greater than 1
+        if (res.data.length > 1) {
+          for (let i = 1; i < res.data.length; i++) {
+            if (node.mem > res.data[i].mem) {
+              node.node = res.data[i].node;
+              node.mem = res.data[i].mem;
+            }
+          }
+        }
+
+        return node.node;
+      });
   },
 };
