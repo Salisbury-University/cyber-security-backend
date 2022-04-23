ae25e2aefcb4281616b84aff6d95f463e5ae3874
lamont1008 <kjenifer1@gulls.salisbury.edu>
2022-04-22 20:27:44 -0400

    testing changes

--- tests/ExerciseService.spec.ts
+++ tests/ExerciseService.spec.ts
@@ -4,25 +4,33 @@ import {app} from '../app'
 import { ExericseService } from '../app/services/ExerciseService';
 import fs from 'fs'
 
+
+
 test.group('ExerciseService', () => {
     var string1 = ""
-    fs.readFile('tests/test-test.md', 'utf8', function(error, f){
-        string1 = f
-        console.log(f)
-    })
-    console.log(string1)
+   string1 = fs.readFileSync('exercises/how-to-parse-markdown.md', 'utf8' ) 
     var username = 'cxarausa'
     var exercise_ID = "1111"
     test('markedLogin', async ({ expect }, done: Function) => {
        var mark = "# test"
        var result = marked.parse(string1)
        var try1 = result.split("<hr>")
-       console.log(result)
        console.log(try1[0] + try1[2])
        var result1 = marked.lexer(string1)
-       console.log(result)
-       var metadata = {
+       for(var i = 0; i < result1.length; i++) {
+           if(result1[i].type == "hr") {
+            var data1 = result1[i+1].raw
+                console.log(result1[i+1].raw)
+                break;
+            }
        }
+       var data2 = {metadata: [data1]}
+        console.log(eval(data2))
+        var j = JSON.parse(JSON.stringify(
+           data1
+        ))
+     
+        console.log(j.title)
         done();
         }).waitForDone();
 })    
