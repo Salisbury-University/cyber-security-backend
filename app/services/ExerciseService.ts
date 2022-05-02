dbec0b7b4833343387e9501b698f89356520fc5d
lamont1008 <kjenifer1@gulls.salisbury.edu>
2022-05-02 14:48:43 -0400

    changed to json format

--- app/services/ExerciseService.ts
+++ app/services/ExerciseService.ts
@@ -24,38 +24,86 @@ async findInfo(exercise_ID, user) {
     
 },
 
-getMetaData(vmid: string): string {
-    const fileLocation = "exercises/how-to-parse-markdown.md";
-    const fileContent = fs.readFileSync(fileLocation, "utf8");
-    const lexer = marked.lexer(fileContent);
-    let content = "";
-    for (let i = 0; i < lexer.length; i++) {
-      if (lexer[i].type == "hr") {
-        content = lexer[i + 1].raw;
-        break;
-      }
-    }
-    console.log(content)
-    const key:string[] = [];
-    const value:string[] = [];
-    //Split by enter and get rid of last
-    const eachRow = content.split("\n");
-    for (let i = 0; i < eachRow.length; i++) {
-      // Split between key and value
-      const eachCol = eachRow[i].split(":");
-      key.push(eachCol[0]);
-      value.push(eachCol[1]);
-    }
+//  getMetaData(vmid: string): string {
+//      const fileLocation = "exercises/how-to-parse-markdown.md";
+//      const fileContent = fs.readFileSync(fileLocation, "utf8");
+//      const lexer = marked.lexer(fileContent);
+//      let content = "";
+//      for (let i = 0; i < lexer.length; i++) {
+//        if (lexer[i].type == "hr") {
+//          content = lexer[i + 1].raw;
+//          break;
+//        }
+//      }
+//      console.log(content)
+//      const key:string[] = [];
+//      const value:string[] = [];
+//      //Split by enter and get rid of last
+//      const eachRow = content.split("\n");
+//      for (let i = 0; i < eachRow.length; i++) {
+//        // Split between key and value
+//        const eachCol = eachRow[i].split(":");
+//        key.push(eachCol[0]);
+//        value.push(eachCol[1]);
+//      }
   
-    let returnString = "{";
-    // Concat the key and value back to be encoded as json in future
-    for (let i = 0; i < key.length - 1; i++) {
-      returnString = returnString.concat('"', key[i], '": ', value[i], ",");
+//      let returnString = "{";
+//      // Concat the key and value back to be encoded as json in future
+//      for (let i = 0; i < key.length - 1; i++) {
+//        returnString = returnString.concat('"', key[i], '": ', value[i], ",");
+//      }
+//      returnString = returnString.substring(0, returnString.length - 2);
+//      returnString = returnString.concat("}");
+//      returnString = JSON.stringify(returnString);
+//      return returnString;
+ // },
+
+ getMetaData(vmid: string): Object {
+  const fileLocation = "exercises/how-to-parse-markdown.md";
+  const fileContent = fs.readFileSync(fileLocation, "utf8");
+  const lexer = marked.lexer(fileContent);
+  let content = "";
+  for (let i = 0; i < lexer.length; i++) {
+    if (lexer[i].type == "hr") {
+      content = lexer[i + 1].text;
+      break;
     }
-    returnString = returnString.substring(0, returnString.length - 2);
-    returnString = returnString.concat("}");
-    returnString = JSON.stringify(returnString);
-    return returnString;
-  },
+  }
+  //Object to store the key and value
+  const metadata = {};
+
+  //Split by enter and get rid of last
+  const eachRow = content.split("\n");
+  for (let i = 0; i < eachRow.length; i++) {
+    // Split between key and value
+    const eachCol = eachRow[i].split(": ");
+    metadata[eachCol[0]] = this.getDataType(eachCol[1]);
+  }
+  return metadata;
+},
+
+getDataType(s: string): any {
+  if (s.startsWith("{") && s.endsWith("}")) {
+    return Object(s);
+  } else if (s.indexOf("/") !== -1 && !isNaN(Date.parse(s))) {
+    return new Date(s).toLocaleString();
+  } else if (!isNaN(parseFloat(s))) {
+    return Number(s);
+  } else if (s.startsWith("[") && s.endsWith("]")) {
+    s = s.substring(1, s.length - 1);
+    const split = s.split(", ");
+    for (let i = 0; i < split.length; i++) {
+      // Gets rid of double quotation
+      split[i] = split[i].substring(1, split[i].length - 1);
+    }
+    return split;
+  } else if (s.toLowerCase() == "true" || s.toLowerCase() == "false") {
+    return Boolean(s);
+  } else {
+    // Gets rid of double quotation
+    s = s.substring(1, s.length - 1);
+    return s;
+  }
+},
 
 }
--- tests/ExerciseService.spec.ts
+++ tests/ExerciseService.spec.ts
@@ -11,46 +11,20 @@ test.group('ExerciseService', () => {
   test('/getMetaData', async({expect}, done:Function)=>{
 
    var j = ExericseService.getMetaData("1111")
+   //console.log(JSON.parse('{"key":0}').key)
+   const id = "200"
 
-   console.log(JSON.parse('{"key":0}').key)
-   console.log(JSON.parse(j).title)
-   console.log(j)
-    //console.log(JSON.parse(j))
-  //   var string1 = ""
-  //  string1 = fs.readFileSync('exercises/how-to-parse-markdown.md', 'utf8' ) 
-  //   var username = 'cxarausa'
-  //   var exercise_ID = "1111"
-  //   test('markedLogin', async ({ expect }, done: Function) => {
-  //      var mark = "# test"
-  //      var result = marked.parse(string1)
-  //      var try1 = result.split("<hr>")
-  //      console.log(try1[0] + try1[2])
-  //      var result1 = marked.lexer(string1)
-  //      for(var i = 0; i < result1.length; i++) {
-  //          if(result1[i].type == "hr") {
-  //           var data1 = result1[i+1].raw
-  //              console.log(result1[i+1].raw)
-  //               break;
-  //           }
-  //      }
+   const status = "incomplete"
 
-      //console.log(data1.split('\n'))
-     // console.log(JSON.parse(JSON.stringify(data1.split('\n'))))
-    //   var d = data1.split('\n')
-    //   var k = "{";
-    //   for(var i = 0; i < d.length; i++) {
-    //     k +=d[i];
-    //     k += ','
-    //   }
-    // k += "}"
-      // console.log(JSON.parse(k))
-      // var data2 = {metadata: [data1]}
-       // console.log(eval(data2))
-       // var j = JSON.parse(JSON.stringify(
-         //  data1
-       // ))
-     
-        //console.log(j.title)
+   var x = {};
+//metadata[eachCol[0]] = this.getDataType(eachCol[1]);
+   x["Excercise_ID"] = id;
+   x["metadata"] = j;
+   x["status"] = status;
+   
+   console.log(JSON.parse(JSON.stringify(x)))
+
         done();
         }).waitForDone();
+      
 })    
