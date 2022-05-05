4803978d098f37e4a3984af484f0c0775d6a291a
JungAn2 <jungan1991@gmail.com>
2022-05-04 22:02:01 -0400

    Reedited json returns

--- app/http/controllers/ExerciseController.ts
+++ app/http/controllers/ExerciseController.ts
@@ -4,8 +4,8 @@ import { VirtualMachineService } from "../../services/VirtualMachineService";
 export const ExcerciseController = {
   async requestVM(req: Request, res: Response, next: NextFunction) {
     try {
-      VirtualMachineService.checkRunningVM(req.user[0].uid);
-      VirtualMachineService.createVM(req.user[0].uid, String(req.params.id));
+      VirtualMachineService.checkRunningVM(req.user.uid);
+      VirtualMachineService.createVM(req.user.uid, String(req.params.id));
     } catch (e) {
       return next(e);
     }
