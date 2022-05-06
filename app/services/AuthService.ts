13ec785acad3fb715b919c67d5ef43c369b2c4ad
JungAn2 <jungan1991@gmail.com>
2022-05-06 19:30:25 -0400

    Fixed having extra bracket

--- app/services/AuthService.ts
+++ app/services/AuthService.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import InvalidCredentialException from '../exceptions/InvalidCredentials';
-import { config } from '../../config'
+import axios from "axios";
+import InvalidCredentialException from "../exceptions/InvalidCredentials";
+import { config } from "../../config";
 import jwt from "jsonwebtoken";
 import JwtMalformedException from "../exceptions/JwtMalformedException";
 
@@ -8,27 +8,26 @@ import JwtMalformedException from "../exceptions/JwtMalformedException";
  * All of the functions regarding authorization
  */
 export const AuthService = {
-
-	/**
-	 * Validates Login for Username and Password
-	 * 
-	 * @param uid username
-	 * @param password password
-	 * @returns token or throws an exception
-	 */
-	async validateLogin(uid: string, password: string): Promise<String> {
-		return await axios.post(config.app.ldap, {
-			uid: uid,
-			password: password
-		})
-			.then(function (response) {
-				return response.data;
-			})
-			.catch(function (error) {
-				throw new InvalidCredentialException();
-			});
-	},
-}
+  /**
+   * Validates Login for Username and Password
+   *
+   * @param uid username
+   * @param password password
+   * @returns token or throws an exception
+   */
+  async validateLogin(uid: string, password: string): Promise<String> {
+    return await axios
+      .post(config.app.ldap, {
+        uid: uid,
+        password: password,
+      })
+      .then(function (response) {
+        return response.data;
+      })
+      .catch(function (error) {
+        throw new InvalidCredentialException();
+      });
+  },
   /**
    * Checks if the token starts with Bearer(JWT token) and a spcae afterward
    *
@@ -66,4 +65,3 @@ export const AuthService = {
     return PAYLOAD;
   },
 };
-
