2b192a37c9db6ea7b1557cc8cac0023f8fd348df
JungAn2 <jungan1991@gmail.com>
2022-05-06 13:55:13 -0400

    Got rid of global types

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -1,17 +1,6 @@
-import { preference } from "@prisma/client";
-
 export {};
 
 declare global {
-  // Declaration of data types
-  type user = {
-    uid: string;
-    iat: number;
-  };
-  type preferenceData = {
-    preference: preference;
-  };
-
   namespace Express {
     interface Request {
       user?: User;
