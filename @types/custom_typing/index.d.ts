df10d4a891355230068e07902df445d0e8ca2eec
JungAn2 <92061566+JungAn2@users.noreply.github.com>
2022-05-10 19:53:04 -0400

    Fixed code that got deleted during conflict

--- @types/custom_typing/index.d.ts
+++ @types/custom_typing/index.d.ts
@@ -1,9 +1,15 @@
+import { preference } from "@prisma/client";
+
 export {};
 
 declare global {
   // Declaration of data types
   type User = {
-    uid: string
-    iat: number
+    uid: string;
+    iat: number;
+  };
+
+  type PreferenceData = {
+    preference: preference;
   };
 }
