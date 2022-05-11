1e348620bc90f00ad0eb6ac943c4b471fc3d7705
lamont1008 <kjenifer1@gulls.salisbury.edu>
2022-05-11 12:33:44 -0500

    added route for authMiddleware

--- app/routes/exercise.ts
+++ app/routes/exercise.ts
@@ -2,6 +2,10 @@ import express from 'express';
 import { ExerciseController } from '../http/controllers/ExerciseController';
 
 const router = express.Router();
+
+import AuthMiddleware from '../http/middleware/AuthMiddleware';
+router.use('/api/v1/auth', AuthMiddleware);
+
 import validate from '../http/middleware/ValidationMiddleware';
 import exerciseSchema from '../schema/ExerciseGetInfo'
 
