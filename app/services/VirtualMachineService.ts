956fba308839bd174dd88cbdf61014b50c51a1e4
JungAn2 <jungan1991@gmail.com>
2022-10-13 20:48:37 -0400

    Changed functionality of assigning and starting VM

--- app/services/VirtualMachineService.ts
+++ app/services/VirtualMachineService.ts
@@ -1,15 +1,61 @@
 import { PrismaClient, VM } from ".prisma/client";
 import { ExerciseService } from "./ExerciseService";
 import axios from "axios";
-import { marked } from "marked";
-import fs, { unlink } from "fs";
 import { config } from "../../config";
 import InsufficientStorageException from "../exceptions/InsufficientStorageException";
 import VirtualMachineConflitException from "../exceptions/VirtualMachineConflictException";
 
 const prisma = new PrismaClient();
 
+const minVMID = 400;
+const maxVMID = 499;
+
 export const VirtualMachineService = {
+  /**
+   * Creates the VM in proxmox
+   *
+   * @param {string} user current user
+   * @param {string} exerciseId vmid of exercise
+   * @param {string} nodeName node name to manual assign load
+   * @return error from function
+   */
+  async createVM(
+    user: string,
+    exerciseId: string,
+    nodeName: string = ""
+  ): Promise<void> {
+    try {
+      let newNode = "";
+      let listOfVM = [];
+      if (nodeName != "") {
+        newNode = nodeName;
+        listOfVM = this.getListofVM();
+      } else {
+        const load = this.selectNodeLoad(exerciseId);
+        newNode = load[0];
+        listOfVM = load[1];
+      }
+
+      // Assign new vmid depending on the current vm
+      const newId = await this.assignNewVMID(
+        exerciseId,
+        user,
+        newNode,
+        listOfVM
+      );
+      this.cloneTemplate(exerciseId, newId);
+
+      if (newNode !== config.app.nodename) {
+        this.migrateTemplate(newId, newNode);
+      }
+
+      this.startVM(newId, newNode);
+      // Wait for 30 sec OS to boot for guest agent to run
+      // await new Promise((r) => setTimeout(r, 30000));
+    } catch (e) {
+      return e;
+    }
+  },
   /**
    * Checks if the user already has a VM running
    * and throw error if exists
@@ -36,30 +82,32 @@ export const VirtualMachineService = {
 
   /**
    * checks the next lowest number to use for newId
-   * All user vm are created from vmid 1000
+   * Min and Max are determined from global variable
    *
-   * @param {number []} vmid array storing vmid
+   * @param {string} exerciseId vmid for the exercise
+   * @param {string} user userId of current user
+   * @param {string} newLoad name of the node
+   * @param {string []} listOfVM list of vm to avoid when assigning vmid
    * @return {number} newId to use for cloning
    */
   async assignNewVMID(
     exerciseId: string,
     user: string,
-    newLoad: string
+    newLoad: string,
+    listOfVM: string[]
   ): Promise<number> {
-    const VM = await prisma.vM.findMany({
-      orderBy: {
-        vmId: "asc",
-      },
-    });
-    let newId = 1000;
-    // Loop through the virtual machine and assign smallest number closest to 1000
-    for (let i = 0; i < VM.length; i++) {
-      if (parseInt(VM[i].vmId) == newId) {
-        newId++;
-      } else {
+    let newId: number = minVMID;
+
+    // Find the list number of VMID without range
+    for (let i = minVMID; i <= maxVMID; i++) {
+      const found = listOfVM.find((element) => i);
+      if (found === undefined) {
+        newId = i;
         break;
       }
     }
+
+    // Get the end time of the vmid
     const metadata: Object = ExerciseService.getMetadata(exerciseId);
     const endTime: Date = this.getVMEndTime(metadata["timelimit"]);
 
@@ -114,6 +162,311 @@ export const VirtualMachineService = {
     const endTime = new Date(currTime + (hours * 60 * 60 + mins * 60 + secs));
     return endTime;
   },
+
+  /**
+   * Starts the virtual machine
+   *
+   * @param {string} vmid id of vm
+   * @param {string} node node where vm is located at
+   */
+  startVM(vmid: string, node: string): void {
+    this.createAxiosWithToken().post(
+      config.app.node.concat(
+        "/api2/json/",
+        node,
+        "/qemu/",
+        vmid,
+        "/status/start"
+      )
+    );
+  },
+
+  /**
+   * Migrates virtual machine to newNode
+   *
+   * @param {string} newId Id of newly created virtual machine
+   * @throw Error during migration
+   * */
+  async migrateTemplate(newId: string, newNode: string): Promise<void> {
+    // Migrate the vm to new node
+    await this.createAxiosWithToken()
+      .post(
+        config.app.node.concat(
+          "/api2/json/nodes",
+          config.app.nodename,
+          "/qemu/",
+          newId,
+          "/migrate?target=",
+          newNode
+        )
+      )
+      .then((res) => {
+        // Get the upid of process
+        this.createAxiosWithToken()
+          .post(
+            config.app.node.concat(
+              "/api2/json/nodes/",
+              newNode,
+              "/tasks",
+              "?vmid=",
+              newId
+            )
+          )
+          .then(async (res) => {
+            // Get status of migration process
+            // Loop until status become stopped
+            const upid = res.data.upid;
+            let loop_process = true;
+            while (loop_process) {
+              this.createAxiosWithToken()
+                .post(
+                  config.app.node.concat(
+                    "/api2/json/nodes/",
+                    newNode,
+                    "/tasks/",
+                    upid,
+                    "/status"
+                  )
+                )
+                .then((res) => {
+                  if (res.data.status === "stopped") {
+                    loop_process = false;
+                  }
+                });
+              // Wait for 1 second before looping
+              await new Promise((r) => setTimeout(r, 1000));
+            }
+          });
+      });
+  },
+
+  /**
+   * Clones the Template to current Node
+   *
+   * @param {string} vmid Virtual Machine ID
+   * */
+  async cloneTemplate(vmid: string, newId: string): Promise<void> {
+    // This part clones the exercise vm to newid
+    await this.createAxiosWithToken()
+      .post(
+        config.app.node.concat(
+          "/api2/json/nodes/",
+          config.app.nodename,
+          "/qemu/",
+          vmid,
+          "/clone?newid=",
+          newId
+        )
+      )
+      .then(async (res) => {
+        // Get the upid of process
+        this.createAxiosWithToken()
+          .post(
+            config.app.node.concat(
+              "/api2/json/nodes/",
+              config.app.nodename,
+              "/tasks",
+              "?vmid=",
+              newId
+            )
+          )
+          .then(async (res) => {
+            // Get status of migration process
+            // Loop until status become stopped
+            const upid = res.data.upid;
+            let loop_process = true;
+            while (loop_process) {
+              this.createAxiosWithToken()
+                .post(
+                  config.app.node.concat(
+                    "/api2/json/nodes/",
+                    config.app.nodename,
+                    "/tasks/",
+                    upid,
+                    "/status"
+                  )
+                )
+                .then((res) => {
+                  if (res.data.status === "stopped") {
+                    loop_process = false;
+                  }
+                });
+              // Wait for 1 second before looping
+              await new Promise((r) => setTimeout(r, 1000));
+            }
+          });
+      });
+  },
+
+  /**
+   * Checks for load of each node and determine based on:
+   * Disk size, VM running, CPU, ram
+   *
+   * @param {string} exerciseId vmid of exercise Id
+   * @return {string} return the node that the template should be cloned to
+   */
+  async selectNodeLoad(exerciseId: string): Promise<Object> {
+    // Gets the list of node (includes node name / cpu usage / disk avaliable)
+    const node: object = [];
+    // This vm will be used for assigning new vmid so that it avoids vmid from
+    //both the database and the ones that are already in the system
+    const vm: string[] = [];
+
+    const axiosNode = this.getListofNodes();
+    for (let i = 0; i < axiosNode.length; i++) {
+      const currNode = axiosNode[i].node;
+      const currDisk = axiosNode[i].maxdisk - axiosNode[i].disk;
+      const currCPU = axiosNode[i].cpu;
+
+      const currVM: string[] = this.getListofVM(axiosNode[i].node);
+      for (let i = 0; i < currVM.length; i++) {
+        vm.push(currVM[i]);
+      }
+      node[i] = {
+        node: currNode,
+        disk: currDisk,
+        cpu: currCPU,
+        vm: currVM,
+      };
+    }
+
+    // Get the size of template
+    const size = this.getSizeTemplate(exerciseId);
+
+    // Check each node disk storage size to see if template can be clones
+    // Otherwise remove it from the array
+    for (let i = 0; i < Object.keys(node).length; i++) {
+      if (size > node[i].disk) {
+        delete node[i];
+      }
+    }
+
+    // If no storage is free, send error
+    if (Object.keys(node).length < 1) {
+      throw new InsufficientStorageException();
+    }
+
+    // Check if the same number of VM's are running on each node
+    // If same, it needs to be checked against CPU usage
+    let sameNumVMRunning = true;
+    let balanceNodeIndex = 0;
+    for (let i = 1; i < Object.keys(node).length; i++) {
+      if (sameNumVMRunning == false) {
+        if (node[balanceNodeIndex].vm.length > node[i].vm.length) {
+          balanceNodeIndex = i;
+        }
+      }
+      if (sameNumVMRunning) {
+        if (node[balanceNodeIndex].vm.length != node[i].vm.length) {
+          sameNumVMRunning = false;
+        }
+      }
+    }
+
+    // If all the same number of running vm check by cpu usage
+    if (sameNumVMRunning) {
+      for (let i = 1; Object.keys(node).length; i++) {
+        if (node[balanceNodeIndex].cpu > node[i].cpu) {
+          balanceNodeIndex = i;
+        }
+      }
+    }
+
+    // Return the name of the least node and list of virtual machine
+    return [node[balanceNodeIndex].node, vm];
+  },
+
+  /**
+   * Gets the disk size of template
+   *
+   * @param {string} vmid The vmid of template
+   * @return {any} size of the template
+   */
+  getSizeTemplate(vmid: string): any {
+    return this.createAxiosWithToken()
+      .post(
+        config.app.node.concat(
+          "/api2/json/nodes/",
+          config.app.nodename,
+          "/qemu/",
+          vmid,
+          "config"
+        )
+      )
+      .then((res) => {
+        for (var key in res.data) {
+          //Checks for ide 0-3 scsi 0-30
+          if (key.includes("ide") || key.includes("scsi")) {
+            const split = res.data[key].split("size=");
+            if (split.length > 2) {
+              continue;
+            } else {
+              // Last letter is G (gigabyte): hit the jackpot
+              const lastLetter = split[1].length - 1;
+              if (split[1][lastLetter] == "G") {
+                const stringSize = split[1].substring(0, lastLetter);
+                return Number(stringSize) * Math.pow(2, 30) * 8;
+              }
+            }
+          }
+        }
+      });
+  },
+
+  /**
+   * Creates axios instance with token for ease of use
+   */
+  createAxiosWithToken(): any {
+    return axios.create({
+      headers: {
+        Authorization: config.app.token,
+      },
+    });
+  },
+
+  /**
+   * Get list of node information from proxmox
+   *
+   * @returns {Object} returns node information of cluster
+   */
+  getListofNodes(): Object {
+    let node: Object = null;
+    this.createAxiosWithToken()
+      .post(config.app.node.concat("/api2/json/nodes"))
+      .then((res) => {
+        node = res.data;
+      });
+    return node;
+  },
+
+  /**
+   * Gets the list of vm across the cluster
+   *
+   * @returns {string []} Returns list of vm
+   */
+  getListofVM(): string[] {
+    const listVM: string[] = [];
+
+    // Get list of nodes in cluster
+    const nodes = this.getListofNodes();
+
+    // Per nodes get list of vm
+    for (let i = 0; i < nodes.length; i++) {
+      // Get list of vm of particular node
+      this.createAxiosWithToken()
+        .post(
+          config.app.node.concat("/api2/json/nodes", nodes[i].node, "/qemu")
+        )
+        .then((res) => {
+          for (let i = 0; i < res.data.length; i++) {
+            listVM.push(res.data[i].vmid);
+          }
+        });
+    }
+
+    return listVM;
+  },
+
   /**
    * Force stop the running VM
    *
@@ -150,238 +503,5 @@ export const VirtualMachineService = {
         )
       )
       .catch((error) => {});
-  },
-
-  /**
-   * Creates the VM
-   *
-   * @param {string} user current user
-   * @param {string} vmid vmid of vm
-   * @return error from function
-   */
-  async createVM(
-    user: string,
-    vmid: string,
-    exerciseId: string
-  ): Promise<void> {
-    try {
-      const load = this.selectNodeLoad(vmid);
-      const newNode = load;
-      const newId = await this.assignNewVMID(exerciseId, user, newNode);
-      this.cloneTemplate(vmid, newId);
-      this.migrateTemplate(newId, newNode);
-      this.startVM(newId, newNode);
-      // Wait for 30 sec OS to boot for guest agent to run
-      await new Promise((r) => setTimeout(r, 30000));
-    } catch (e) {
-      return e;
-    }
-  },
-
-  /**
-   * Starts the virtual machine
-   *
-   * @param {string} vmid id of vm
-   * @param {string} node node where vm is located at
-   */
-  startVM(vmid: string, node: string) {
-    this.createAxiosWithToken().post(
-      config.app.node.concat(
-        "/api2/json/",
-        node,
-        "/qemu/",
-        vmid,
-        "/status/start"
-      )
-    );
-  },
-
-  /**
-   * Migrates virtual machine to newNode
-   *
-   * @param {string} newId Id of newly created virtual machine
-   * @throw Error during migration
-   * */
-  migrateTemplate(newId: string, newNode: string): void {
-    this.createAxiosWithToken()
-      .post(
-        config.app.node.concat(
-          "/api2/json/nodes",
-          config.app.node,
-          "/qemu/",
-          newId,
-          "/migrate?target=",
-          newNode
-        )
-      )
-      .catch((error) => {
-        // Throw error if fails to migrate
-      });
-  },
-
-  /**
-   * Clones the Template to current Node
-   *
-   * @param {string} vmid Virtual Machine ID
-   * */
-  cloneTemplate(vmid: string, newId: string): void {
-    this.createAxiosWithToken()
-      .post(
-        config.app.node.concat(
-          "/api2/json/nodes/",
-          config.app.nodename,
-          "/qemu/",
-          vmid,
-          "/clone?newid=",
-          newId
-        )
-      )
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-
-  /**
-   * Checks for load of each node and determine based on:
-   * Disk size, VM running, CPU, ram
-   *
-   * @param {string} vmid number of vmid to clone
-   * @return {string} return the node that the template should be cloned to
-   */
-  async selectNodeLoad(
-    vmid: string,
-    exerciseId: string,
-    user: string
-  ): Promise<string> {
-    // Gets the list of node (includes node name / cpu usage / disk avaliable)
-    const node: object = [];
-
-    this.createAxiosWithToken()
-      .post(config.app.node.concat("/api2/json/nodes"))
-      .then((res) => {
-        for (let i = 0; i < res.data.length; i++) {
-          const currNode = res.data[i].node;
-          // Calculate disk avaliable and push to array
-          const currDisk = res.data[i].maxdisk - res.data[i].disk;
-          const currCPU = res.data[i].cpu;
-
-          // Get vm running per node
-          this.createAxiosWithToken()
-            .post(
-              config.app.node.concat(
-                "/api2/json/nodes/",
-                res.data[i].node,
-                "/qemu"
-              )
-            )
-            .then((res) => {
-              const currVM = [];
-              for (let j = 0; j < res.data.length; j++) {
-                if (res.data[i].vmid >= 1000) {
-                  currVM.push(res.data[i].vmid);
-                }
-              }
-              node[i] = {
-                node: currNode,
-                disk: currDisk,
-                cpu: currCPU,
-                vm: currVM,
-              };
-            });
-        }
-      });
-
-    // Get the size of template
-    const size = this.getSizeTemplate(vmid);
-
-    // Check each node disk storage size to see if template can be clones
-    // Otherwise remove it from the array
-    for (let i = 0; i < Object.keys(node).length; i++) {
-      if (size > node[i].disk) {
-        delete node[i];
-      }
-    }
-
-    // If no storage is free, send error
-    if (Object.keys(node).length < 1) {
-      throw new InsufficientStorageException();
-    }
-
-    // Check if the number of VM running are the same
-    // If same, it needs to be checked against CPU usage
-    let sameNumVMRunning = true;
-    let balanceNodeIndex = 0;
-    for (let i = 1; i < Object.keys(node).length; i++) {
-      if (sameNumVMRunning == false) {
-        if (node[balanceNodeIndex].vm.length > node[i].vm.length) {
-          balanceNodeIndex = i;
-        }
-      }
-      if (sameNumVMRunning) {
-        if (node[balanceNodeIndex].vm.length != node[i].vm.length) {
-          sameNumVMRunning = false;
-        }
-      }
-    }
-
-    // If all the same number of running vm check by cpu usage
-    if (sameNumVMRunning) {
-      for (let i = 1; Object.keys(node).length; i++) {
-        if (node[balanceNodeIndex].cpu > node[i].cpu) {
-          balanceNodeIndex = i;
-        }
-      }
-    }
-
-    // Return the name of the least node
-    return node[balanceNodeIndex].node;
-  },
-
-  /**
-   * Gets the disk size of template
-   *
-   * @param {string} vmid The vmid of template
-   * @return {any} size of the template
-   */
-  getSizeTemplate(vmid: string): any {
-    return this.createAxiosWithToken()
-      .post(
-        config.app.node.concat(
-          "/api2/json/nodes/",
-          config.app.node,
-          "/qemu/",
-          vmid,
-          "config"
-        )
-      )
-      .then((res) => {
-        for (var key in res.data) {
-          //Checks for ide 0-3 scsi 0-30
-          if (key.includes("ide") || key.includes("scsi")) {
-            const split = res.data[key].split("size=");
-            if (split.length > 2) {
-              continue;
-            } else {
-              // Last letter is G (gigabyte): hit the jackpot
-              const lastLetter = split[1].length - 1;
-              if (split[1][lastLetter] == "G") {
-                const stringSize = split[1].substring(0, lastLetter);
-                return Number(stringSize) * Math.pow(2, 30) * 8;
-              }
-            }
-          }
-        }
-      });
-  },
-
-  /**
-   * Creates axios instance with token for ease of use
-   */
-  createAxiosWithToken(): any {
-    return axios.create({
-      headers: {
-        Authorization: config.app.token,
-      },
-    });
   },
 };
