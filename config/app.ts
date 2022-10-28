2494072a04afbc0e9aa150279f70a64481f08413
JungAn2 <jungan1991@gmail.com>
2022-10-28 14:00:55 -0400

    Changed the config to be more proffessional

--- config/app.ts
+++ config/app.ts
@@ -6,11 +6,10 @@ export default {
 	The port the web server listens on for connections.
 	*/
   port: process.env.PORT ? process.env.PORT : 3000,
-  ldap: process.env.LDAP ? process.env.LDAP : "IDK",
-  ticket_id: process.env.TICKET_ID ? process.env.TICKET_ID : "wrong",
-  ticket_uuid: process.env.TICKET_UUID ? process.env.TICKET_UUID : "hahaha",
-  url: process.env.URL ? process.env.URL : "move along",
-  nodename: process.env.NODENAME ? process.env.NODENAME : "don't mind me",
+  ldap: process.env.LDAP ? process.env.LDAP : "",
+  ticket_id: process.env.TICKET_ID ? process.env.TICKET_ID : "",
+  ticket_uuid: process.env.TICKET_UUID ? process.env.TICKET_UUID : "",
+  url: process.env.URL ? process.env.URL : "",
   token: "PVEAPIToken=".concat(
     process.env.TICKET_ID,
     "=",
