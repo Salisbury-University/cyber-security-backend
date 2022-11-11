ff72c7d302cfd9c2f5028a106d4e87f39795b751
JungAn2 <jungan1991@gmail.com>
2022-11-11 13:03:33 -0500

    Added comments to the exercise routes

--- app/routes/auth.ts
+++ app/routes/auth.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { AuthController } from "../http/controllers/AuthController";
-import { IndexController } from "../http/controllers/IndexController";
 
 const router = express.Router();
 
@@ -8,7 +7,7 @@ const router = express.Router();
 import validate from "../http/middleware/ValidationMiddleware";
 import loginSchema from "../schema/AuthLoginPost";
 
+// Login route
 router.post("/login", validate(loginSchema), AuthController.login);
-router.get("/", IndexController.index);
 
 export default router;
--- app/routes/exercise.ts
+++ app/routes/exercise.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 import validate from "../http/middleware/ValidationMiddleware";
 import exerciseSchema from "../schema/ExerciseGetInfo";
 
+// Get the exercise information
 router.get("/:id", validate(exerciseSchema), ExerciseController.getInfo);
 
 export default router;
